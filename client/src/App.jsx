fc1a191345631c9f1ed39330d41e3b1ce42c4961
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-02-22 16:49:24 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import ChannelListContainer from "./components/ChannelListContainer";
+import ChannelContainer from "./components/ChannelContainer";
+
+const apiKey = process.env.REACT_APP_STREAM_API_KEY;
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
