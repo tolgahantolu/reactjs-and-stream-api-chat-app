a5b9bf813e704c00fb9515e91f5ef1b75c3023eb
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-02-26 10:44:22 +0300

    auth: client-side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
-import { ChannelContainer, ChannelListContainer } from "./components";
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
 import "./App.css";
 
 const apiKey = process.env.REACT_APP_STREAM_API_KEY;
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
