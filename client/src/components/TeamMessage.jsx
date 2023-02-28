cd67cdc296443359e92b83b4ad3648bc70c98a2b
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-02-28 11:18:33 +0300

    channel container

---
+++ client/src/components/TeamMessage.jsx
@@ -0,0 +1,63 @@
+//import React from "react";
+//
+//const TeamMessage = () => {
+//  return <div>TeamMessage</div>;
+//};
+//
+//export default TeamMessage;
+
+import React, { useRef } from "react";
+import {
+  Attachment,
+  Avatar,
+  messageHasReactions,
+  MessageOptions,
+  MessageRepliesCountButton,
+  MessageStatus,
+  MessageText,
+  MessageTimestamp,
+  ReactionSelector,
+  SimpleReactionsList,
+  useMessageContext,
+} from "stream-chat-react";
+
+const TeamMessage = () => {
+  const {
+    isReactionEnabled,
+    message,
+    reactionSelectorRef,
+    showDetailedReactions,
+  } = useMessageContext();
+
+  const messageWrapperRef = useRef(null);
+
+  const hasReactions = messageHasReactions(message);
+  const hasAttachments = message.attachments && message.attachments.length > 0;
+
+  return (
+    <div className="message-wrapper">
+      <Avatar image={message.user?.image} />
+      <div className="message-wrapper-content">
+        <MessageOptions messageWrapperRef={messageWrapperRef} />
+        <div className="message-header">
+          <div className="message-header-name">{message.user?.name}</div>
+          <div className="message-header-timestamp">
+            <MessageTimestamp />
+          </div>
+        </div>
+        {showDetailedReactions && isReactionEnabled && (
+          <ReactionSelector ref={reactionSelectorRef} />
+        )}
+        <MessageText />
+        <MessageStatus />
+        {hasAttachments && <Attachment attachments={message.attachments} />}
+        {hasReactions && !showDetailedReactions && isReactionEnabled && (
+          <SimpleReactionsList />
+        )}
+        <MessageRepliesCountButton reply_count={message.reply_count} />
+      </div>
+    </div>
+  );
+};
+
+export default TeamMessage;
